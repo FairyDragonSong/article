48bd12a68c5026353ac0adbdf65d2c20975917dc
FairyDragonSong <31773171+FairyDragonSong@users.noreply.github.com>
2021-06-20 17:43:12 +0800

    --
    --

--- Assets/ClothesSimulate/Scripts/Spring.cs
+++ Assets/ClothesSimulate/Scripts/Spring.cs
@@ -36,9 +36,18 @@ public class Spring : MonoBehaviour
     /// <summary>
     /// 弹簧初始长度
     /// </summary>
-    public float L0 = 1;
+    public float L0 = 3;
 
-    private Vector3 oriCurPos;
+    /// <summary>
+    /// 弹簧的形变范围
+    /// </summary>
+    public float Lmin = 1f;
+    public float Lmam = 5f;
+
+    /// <summary>
+    /// 临界值
+    /// </summary>
+    public float Lcritical = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -47,6 +56,17 @@ public class Spring : MonoBehaviour
         info1.prePos = go1.transform.position;
         info2.curPos = go2.transform.position;
         info2.prePos = go2.transform.position;
+
+        var queue = new CoroutineQueue(2, StartCoroutine);
+        queue.Run(DelayAddForce());
+    }
+
+    IEnumerator DelayAddForce()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log("===============");
+        go1.transform.Translate(-go1.transform.right * 0.1f);
+        go2.transform.Translate(go2.transform.right * 0.1f);
     }
 
     // Update is called once per frame
@@ -70,39 +90,58 @@ public class Spring : MonoBehaviour
         Vector3 offsetVector3 = realMoveDis * inertia * 0.9f;
 
         // go.transform.position = offsetVector3 + info.curPos;
-        oriCurPos = info.curPos;
         info.curPos = offsetVector3 + info.curPos;
     }
+
+    public bool rever = false;
 
     private void CheckSpring(GameObject go1, particle info1, GameObject go2, particle info2)
     {
         Vector3 delLen = go1.transform.position - go2.transform.position;
 
-        // 乘上 0.9
-        Vector3 speed1 = (go1.transform.position - info1.prePos) * 0.99f / Time.deltaTime;
+        Vector3 speed1 = (go1.transform.position - info1.prePos) / Time.deltaTime;
         Vector3 speed2 = (go2.transform.position - info2.prePos) / Time.deltaTime;
 
-        Vector3 elasF1 = -(delLen.magnitude - L0) * elastic * delLen / delLen.magnitude - damp * (speed1 - speed2);
+        if (delLen.magnitude < Lmin)
+        {
+            // 弹簧异常
+            float iExtentOfCritical = (Lmin - delLen.magnitude) / (Lmin - Lcritical);
 
-        elasF1 = elasF1 * 0.1f;
+            if (iExtentOfCritical <= 0.99)
+            {
+                Debug.Log(iExtentOfCritical);
 
-        Vector3 elasF2 = - elasF1;
+                rever = true;
+                iExtentOfCritical = iExtentOfCritical - 0.01f;
+            }
 
-        float mass1 = 1f;
-        float mass2 = 1f;
+            speed1 = info1.speed * (1 - iExtentOfCritical) * (rever ? -1 : 1);
+            speed2 = info2.speed * (1 - iExtentOfCritical) * (rever ? -1 : 1);
 
-        Vector3 a1 = new Vector3(elasF1.x / mass1, elasF1.y / mass1, elasF1.z / mass1);
-        Vector3 a2 = new Vector3(elasF2.x / mass2, elasF2.y / mass2, elasF2.z / mass2);
+        }
+        else
+        {
+            rever = false;
+            // 阻尼力和弹力计算
+            Vector3 elasF1;
+            Vector3 elasF2;
 
-        Debug.Log(a1);
-        Debug.Log(a2);
+            elasF1 = -(delLen.magnitude - L0) * elastic * delLen / delLen.magnitude - damp * (speed1 - speed2);
+            elasF2 = -elasF1;
 
-        speed1 += a1 * Time.deltaTime;
-        speed2 += a2 * Time.deltaTime;
+            // 质量
+            float mass1 = 1f;
+            float mass2 = 1f;
+            // 加速度
+            Vector3 a1 = elasF1 / mass1;
+            Vector3 a2 = elasF2 / mass2;
 
-        Debug.Log(a1 * Time.deltaTime);
-        Debug.Log(a2 * Time.deltaTime);
-        Debug.Log("=========================");
+            speed1 += a1 * Time.deltaTime;
+            speed2 += a2 * Time.deltaTime;
+
+            info1.speed = speed1;
+            info2.speed = speed2;
+        }
 
         Vector3 offset1 = speed1 * Time.deltaTime;
         Vector3 offset2 = speed2 * Time.deltaTime;
@@ -111,6 +150,7 @@ public class Spring : MonoBehaviour
         go1.transform.position = info1.curPos + offset1;
         go2.transform.position = info2.curPos + offset2;
 
+        // 数据保存
         info1.prePos = info1.curPos;
         info2.prePos = info2.curPos;
         info1.curPos = go1.transform.position;
